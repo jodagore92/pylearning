43233d8d619039c1a43f761b3d484994d13734cf
Jose David Gomez <jodagore92@gmail.com>
2023-10-09 10:52:35 -0500

    Suma de dos numeros

---
+++ src/exce01.py
@@ -0,0 +1,6 @@
+# Calcula la suma de dos numeros
+
+numero1 = int(input("Escriba un numero entero: "))
+numero2 = int(input("Escriba otro numero entero: "))
+resultado = numero1 + numero2
+print("(" + str(numero1) + ") + (" + str(numero2) + ") = (" + str(resultado) + ")")
